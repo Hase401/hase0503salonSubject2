e1b32e3107bca50bf01d6472c109550718872bfc
akio0911 <akio0911@gmail.com>
2021-05-04 23:12:30 +0900

    feedback

--- subject_2/subject_2/ViewController.swift
+++ subject_2/subject_2/ViewController.swift
@@ -9,55 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var state: Int = 0
-    
     @IBOutlet private weak var inputedNumber1ByTextField: UITextField!
     @IBOutlet private weak var inputedNumber2ByTextField: UITextField!
     @IBOutlet private weak var segmentedControl: UISegmentedControl!
     @IBOutlet private weak var resultNumberLabel: UILabel!
-    @IBAction private func chooseCalculationBySegmentedControl(_ sender: UISegmentedControl) {
-        switch sender.selectedSegmentIndex {
-        case 0:
-            state = 0
-            break
-        case 1:
-            state = 1
-            break
-        case 2:
-            state = 2
-            break
-        case 3:
-            state = 3
-            break
-        default:
-            break
-        }
-    }
+
     @IBAction private func calculateButton(_ sender: UIButton) {
         let number1 = Float(inputedNumber1ByTextField.text!) ?? 0
         let number2 = Float(inputedNumber2ByTextField.text!) ?? 0
         
-        switch state {
+        switch segmentedControl.selectedSegmentIndex {
         case 0:
             let result = number1 + number2
             resultNumberLabel.text = "\(result)"
-            break
         case 1:
             let result = number1 - number2
             resultNumberLabel.text = "\(result)"
-            break
         case 2:
             let result = number1 * number2
             resultNumberLabel.text = "\(result)"
-            break
         case 3:
             if number2 == 0 {
                 resultNumberLabel.text = "割る数には0以外を入力してください"
-                break
             } else {
                 let result = number1 / number2
                 resultNumberLabel.text = "\(result)"
-                break
             }
         default:
             break
